cb97b33d0252be2a7e63a53f3cdc6ceda3c5878f
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2023-04-17 17:03:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+namespace filehandling2
+{
+class Program
+{
+    static void Main()
+    {
+        string path= @"C:\Users\Sumon_\OneDrive\Desktop\Supu.txt";
+    
+    
+        Console.WriteLine("Enter Text: ");
+        string input1= Console.ReadLine();
+        string input2= Console.ReadLine();
+        
+        try
+        {
+            TextWriter writer = new StreamWriter(path);
+            writer.WriteLine(input1);
+            writer.WriteLine(input2);
+            writer.Close();
+        }
+        catch(IOException ex)
+        {
+            Console.WriteLine("Invalid "+ex.Message);
+        }
+
+        Console.WriteLine("Reading the contents of the file");
+
+        try
+        {
+            TextReader reader= new StreamReader(path);
+            string line= reader.ReadLine();
+            Console.WriteLine($"The contents are {line}");
+            reader.Close();
+        }
+        catch (IOException ioe)
+        {
+            Console.WriteLine(ioe.Message);
+        }
+    }
+}
+}
