d9ccca9a77f648971420bcd099ea3ff98507a03d
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2023-04-17 18:00:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace filehandling6
+{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteFile();
+        ReadFile();
+    }
+    public static void WriteFile()
+    {
+        string path=@"C:\Users\Sumon_\OneDrive\Desktop\myfile\Surya.txt";
+        StreamWriter writer = new StreamWriter(path);
+        writer.WriteLine("Hello SuryaKumar Yadav");
+        writer.Close();
+
+    }
+    public static void ReadFile()
+    {
+        string path=@"C:\Users\Sumon_\OneDrive\Desktop\myfile\Surya.txt";
+        StreamReader reader= new StreamReader(path);
+        string res= reader.ReadLine();
+        Console.WriteLine(res);
+        reader.Close();
+    }
+}
+}
