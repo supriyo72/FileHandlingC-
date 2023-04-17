a914e753b4decfd5baac639fdd43cac859f0931f
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2023-04-17 17:01:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace filehandling1
+{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string path= @"C:\Users\Sumon_\OneDrive\Desktop\Raja.txt";
+        Console.WriteLine(path);
+        
+        if(File.Exists(path))
+        {
+            Console.WriteLine("Found");
+        }
+        else{
+            Console.WriteLine("Not Found");
+            File.Create(path);
+        }
+    }
+}
+}
